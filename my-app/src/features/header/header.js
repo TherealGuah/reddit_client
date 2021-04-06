a7b2885424e3ce25cf39d7cbe16d5b7be2fd6414
TherealGuah <64426693+TherealGuah@users.noreply.github.com>
2021-04-06 19:31:52 +0100

    refactored header

--- my-app/src/features/header/header.js
+++ my-app/src/features/header/header.js
@@ -6,7 +6,7 @@ export function Header() {
     
     return (
         <header>
-            <h1 className="header">All good here</h1>
+            <h1>All good here</h1>
         </header>
     );
 };
