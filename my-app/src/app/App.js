472593c3893a605ce347d1eef8fb56a54368de93
TherealGuah <64426693+TherealGuah@users.noreply.github.com>
2021-04-06 15:52:24 +0100

    connected Header to App

--- my-app/src/app/App.js
+++ my-app/src/app/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import './App.css';
+import { Header } from '../features/header/header';
 
 function App() {
-  return (<div></div>);
+  return (
+  <>
+    <Header /> 
+  </>);
 }
 
 export default App;
---
+++ my-app/src/features/header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+export function Header() {
+    
+    
+    return (
+        <header>
+            <h1 className="header">All good here</h1>
+        </header>
+    );
+};
