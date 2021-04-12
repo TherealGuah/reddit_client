f8f2edea79c93f69d96f1f4d516f82067334e3f6
TherealGuah <64426693+TherealGuah@users.noreply.github.com>
2021-04-12 22:31:47 +0100

    basic posts structure and data established and some CSS for responsive design

--- my-app/src/features/main/posts/postsList.js
+++ my-app/src/features/main/posts/postsList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-
+import './postsList.css'
 import { getSubredditPosts } from '../../../app/API/Reddit';
 import { selectSubReddit } from '../subReddits/subRedditsSlice';
 import { useDispatch, useSelector } from 'react-redux';
@@ -20,16 +20,28 @@ export function PostsList() {
     useEffect( () => {
         getSubredditPosts(activeSubReddit)
         .then( jsonData => {
-            // console.log(jsonData);
+            //console.log(jsonData);
             dispatch(addPosts(jsonData))
         })
-    }, [activeSubReddit, dispatch])
+    }, [activeSubReddit, dispatch]);
+    
+    function handleImgError({target}) {
+        target.style.display = "none";
+    };
 
+
+    console.log(posts);
     return (
-        <div>
-            <Link to="/Post">
-                <p>{posts[0].title}</p>
-            </Link>
+        <div id="postsItem">
+            {posts.map( post => (
+                <div className="singlePost" key={post.id}>
+                    <Link to="/Post">
+                        <h1>{post.title}</h1>
+                    </Link>
+                    <p >{post.selftext}</p>
+                    <img src={post.url} alt="" onError={handleImgError}/>
+                </div>)
+            )}
         </div>
     );
 };
